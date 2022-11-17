8e88be174cbda62c6cddd07403bd6fd2fae1d093
MPLace1 <matthew.place1@nhs.net>
2022-11-17 15:35:42 +0000

    12. DELETE /api/comments/:comment_id complete

--- models/bg.model.js
+++ models/bg.model.js
@@ -147,3 +147,23 @@ exports.fetchUsers = () => {
             return users.rows
         })
 }
+
+exports.selectCommentByID = (comment_id) => {
+    return db
+        .query(
+            `SELECT * FROM comments WHERE review_id = $1 ORDER BY created_at DESC`,
+            [comment_id])
+        .then((comment) => {
+            return comment.rows[0]
+        })
+}
+
+exports.removeCommentByID = (comment_id) => {
+    return db   
+        .query(
+            `DELETE FROM comments WHERE comment_id = $1`, [comment_id]
+        )
+        .then((comment) => {
+            return comment.rows[0]
+        })
+}
